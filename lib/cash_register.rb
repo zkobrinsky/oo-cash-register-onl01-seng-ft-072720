0a43e46550599b90b9597579c65edd7596567186
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-08 15:07:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,6 +4,7 @@ class CashRegister
 attr_accessor :total, :discount
 
 
+@@last_item_price = ""
 
 def initialize(discount = 0)
   @@items = []
@@ -14,6 +15,7 @@ end
 def add_item(item, price, quantity = 1)
   quantity.times {@@items << item}
   @total += price * quantity
+  @@last_item_price = price
 end
 
 def apply_discount
